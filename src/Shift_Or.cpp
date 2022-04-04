accabb2a887f22f15f1129fbc66291a96dcaae8f
pedrobet <pedro@meira.com>
2022-04-04 20:43:05 -0300

    right shift or

--- src/Shift_Or.cpp
+++ src/Shift_Or.cpp
@@ -1,6 +1,9 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <cstring>
+#include <fstream>
+#include "algorithms.hpp"
 using namespace std;
 
 //shift or
@@ -14,17 +17,61 @@ vector<long> pm(string pat) {
     }
     return mask;
 }
-void shift_or(string txt, int patlen, vector<long> mask, vector<int>& result, string pat) {
+bool shift_or(string txt, int patlen, vector<long> mask, int& result, string pat) {
+    bool find = false;
     long S = ~1;
-    for (int i = 0; i < txt.length(); i++) {
+    for (int i = 0; i < (int)txt.length(); i++) {
         S |= mask[txt[i]];
         S <<= 1;
         if ((S & (1L << patlen)) == 0) {
-            result.push_back(i - patlen + 1);
+            result++;
+            find = true;
         }
     }
+    return find;
 }
 
-void patternSO(string txt, string pat, vector<int>& result) {
+int patternSO(string txt, string pat) {
+    int result = 0;
     shift_or(txt, pat.length(), pm(pat), result, pat);
+    // if (result >0) {
+    //     cout << "total ocurrances: " << result;
+    //     cout << "\nthey occured at the folowing places: ";
+    //     for (int i = 0; i < result; i++) {
+    //         cout << result[i] << ";";
+    //     }
+    // }
+    // else {
+    //     cout << "no ocurrances";
+    // }
+    return result;
+}
+
+void run_shift_or(vector<string> txt_set, vector<string> pat_set, bool c) {
+    vector<vector<int>> lps_set;
+    int l_txt_set = (int)txt_set.size();
+    int l_pat_set = (int)pat_set.size();
+    vector<int> count(l_pat_set, 0);
+    for (int i = 0; i < l_txt_set; i++) {
+        ifstream txt(txt_set[i]);
+        string line;
+        int n_line = 1;
+        while (!txt.eof()) {
+            getline(txt, line);
+            bool find = false;
+            for (int j = 0; j < l_pat_set; j++) {
+                find = shift_or(line, pat_set[j].length(), pm(pat_set[j]), count[j], pat_set[j]);
+            }
+            if (find and !c) {
+                cout << "line " << n_line << ": " << line << endl;
+            }
+            n_line++;
+        }
+        txt.close();
+    }
+    if (c) {
+        for (int i = 0; i < l_pat_set; i++) {
+            cout << pat_set[i] << ": " << count[i] << endl;
+        }
+    }
 }
